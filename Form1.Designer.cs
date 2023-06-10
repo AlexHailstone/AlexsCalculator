fba261e60c83d9ab51d83d99614c7bd393c2ceae
Solex <alexander.ramsbey@gmail.com>
2023-06-10 14:36:12 -0400

    Numbers and Output created

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AlexsCalculator
 {
-	partial class Calculator
+	partial class CalculatorWindow
 	{
 		/// <summary>
 		///  Required designer variable.
@@ -28,20 +28,285 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			Seven = new Button();
+			Eight = new Button();
+			Nine = new Button();
+			Four = new Button();
+			Five = new Button();
+			Six = new Button();
+			One = new Button();
+			Two = new Button();
+			Three = new Button();
+			Decimal = new Button();
+			Zero = new Button();
+			button3 = new Button();
+			Equals = new Button();
+			button5 = new Button();
+			button6 = new Button();
+			button1 = new Button();
+			ClearEntry = new Button();
+			Clear = new Button();
+			textBox1 = new TextBox();
+			Output = new Label();
 			SuspendLayout();
 			// 
-			// Calculator
+			// Seven
+			// 
+			Seven.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			Seven.Location = new Point(34, 275);
+			Seven.Name = "Seven";
+			Seven.Size = new Size(60, 52);
+			Seven.TabIndex = 0;
+			Seven.Text = "7";
+			Seven.UseVisualStyleBackColor = true;
+			Seven.Click += button1_Click;
+			// 
+			// Eight
+			// 
+			Eight.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			Eight.Location = new Point(100, 275);
+			Eight.Name = "Eight";
+			Eight.Size = new Size(60, 52);
+			Eight.TabIndex = 1;
+			Eight.Text = "8";
+			Eight.UseVisualStyleBackColor = true;
+			// 
+			// Nine
+			// 
+			Nine.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			Nine.Location = new Point(166, 275);
+			Nine.Name = "Nine";
+			Nine.Size = new Size(60, 52);
+			Nine.TabIndex = 2;
+			Nine.Text = "9";
+			Nine.UseVisualStyleBackColor = true;
+			// 
+			// Four
+			// 
+			Four.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			Four.Location = new Point(34, 333);
+			Four.Name = "Four";
+			Four.Size = new Size(60, 52);
+			Four.TabIndex = 3;
+			Four.Text = "4";
+			Four.UseVisualStyleBackColor = true;
+			// 
+			// Five
+			// 
+			Five.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			Five.Location = new Point(100, 333);
+			Five.Name = "Five";
+			Five.Size = new Size(60, 52);
+			Five.TabIndex = 4;
+			Five.Text = "5";
+			Five.UseVisualStyleBackColor = true;
+			// 
+			// Six
+			// 
+			Six.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			Six.Location = new Point(166, 333);
+			Six.Name = "Six";
+			Six.Size = new Size(60, 52);
+			Six.TabIndex = 5;
+			Six.Text = "6";
+			Six.UseVisualStyleBackColor = true;
+			// 
+			// One
+			// 
+			One.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			One.Location = new Point(34, 391);
+			One.Name = "One";
+			One.Size = new Size(60, 52);
+			One.TabIndex = 6;
+			One.Text = "1";
+			One.UseVisualStyleBackColor = true;
+			// 
+			// Two
+			// 
+			Two.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			Two.Location = new Point(100, 391);
+			Two.Name = "Two";
+			Two.Size = new Size(60, 52);
+			Two.TabIndex = 7;
+			Two.Text = "2";
+			Two.UseVisualStyleBackColor = true;
+			// 
+			// Three
+			// 
+			Three.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			Three.Location = new Point(166, 391);
+			Three.Name = "Three";
+			Three.Size = new Size(60, 52);
+			Three.TabIndex = 8;
+			Three.Text = "3";
+			Three.UseVisualStyleBackColor = true;
+			// 
+			// Decimal
+			// 
+			Decimal.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			Decimal.Location = new Point(166, 449);
+			Decimal.Name = "Decimal";
+			Decimal.Size = new Size(60, 52);
+			Decimal.TabIndex = 14;
+			Decimal.Text = ".";
+			Decimal.UseVisualStyleBackColor = true;
+			// 
+			// Zero
+			// 
+			Zero.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			Zero.Location = new Point(34, 449);
+			Zero.Name = "Zero";
+			Zero.Size = new Size(126, 52);
+			Zero.TabIndex = 13;
+			Zero.Text = "0";
+			Zero.UseVisualStyleBackColor = true;
+			Zero.Click += button2_Click;
+			// 
+			// button3
+			// 
+			button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			button3.Location = new Point(232, 275);
+			button3.Name = "button3";
+			button3.Size = new Size(60, 52);
+			button3.TabIndex = 12;
+			button3.Text = "*";
+			button3.UseVisualStyleBackColor = true;
+			// 
+			// Equals
+			// 
+			Equals.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			Equals.Location = new Point(232, 449);
+			Equals.Name = "Equals";
+			Equals.Size = new Size(60, 52);
+			Equals.TabIndex = 11;
+			Equals.Text = "=";
+			Equals.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			button5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			button5.Location = new Point(232, 333);
+			button5.Name = "button5";
+			button5.Size = new Size(60, 52);
+			button5.TabIndex = 10;
+			button5.Text = "-";
+			button5.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			button6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			button6.Location = new Point(232, 391);
+			button6.Name = "button6";
+			button6.Size = new Size(60, 52);
+			button6.TabIndex = 9;
+			button6.Text = "+";
+			button6.UseVisualStyleBackColor = true;
+			button6.Click += button6_Click;
+			// 
+			// button1
+			// 
+			button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			button1.Location = new Point(232, 217);
+			button1.Name = "button1";
+			button1.Size = new Size(60, 52);
+			button1.TabIndex = 15;
+			button1.Text = "/";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click_1;
+			// 
+			// ClearEntry
+			// 
+			ClearEntry.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			ClearEntry.Location = new Point(166, 217);
+			ClearEntry.Name = "ClearEntry";
+			ClearEntry.Size = new Size(60, 52);
+			ClearEntry.TabIndex = 16;
+			ClearEntry.Text = "CE";
+			ClearEntry.UseVisualStyleBackColor = true;
+			// 
+			// Clear
+			// 
+			Clear.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+			Clear.Location = new Point(34, 217);
+			Clear.Name = "Clear";
+			Clear.Size = new Size(126, 52);
+			Clear.TabIndex = 17;
+			Clear.Text = "C";
+			Clear.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			textBox1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+			textBox1.Location = new Point(34, 141);
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(257, 39);
+			textBox1.TabIndex = 18;
+			// 
+			// Output
+			// 
+			Output.AutoSize = true;
+			Output.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+			Output.Location = new Point(34, 95);
+			Output.Name = "Output";
+			Output.Size = new Size(95, 32);
+			Output.TabIndex = 19;
+			Output.Text = "Output";
+			Output.Click += label1_Click;
+			// 
+			// CalculatorWindow
 			// 
 			AutoScaleDimensions = new SizeF(8F, 20F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(611, 513);
-			Name = "Calculator";
+			ClientSize = new Size(327, 513);
+			Controls.Add(Output);
+			Controls.Add(textBox1);
+			Controls.Add(Clear);
+			Controls.Add(ClearEntry);
+			Controls.Add(button1);
+			Controls.Add(Decimal);
+			Controls.Add(Zero);
+			Controls.Add(button3);
+			Controls.Add(Equals);
+			Controls.Add(button5);
+			Controls.Add(button6);
+			Controls.Add(Three);
+			Controls.Add(Two);
+			Controls.Add(One);
+			Controls.Add(Six);
+			Controls.Add(Five);
+			Controls.Add(Four);
+			Controls.Add(Nine);
+			Controls.Add(Eight);
+			Controls.Add(Seven);
+			Name = "CalculatorWindow";
 			StartPosition = FormStartPosition.CenterScreen;
 			Text = "Alex's Calculator";
 			Load += Form1_Load;
 			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
+
+		private Button Seven;
+		private Button Eight;
+		private Button Nine;
+		private Button Four;
+		private Button Five;
+		private Button Six;
+		private Button One;
+		private Button Two;
+		private Button Three;
+		private Button Decimal;
+		private Button Zero;
+		private Button button3;
+		private Button Equals;
+		private Button button5;
+		private Button button6;
+		private Button button1;
+		private Button ClearEntry;
+		private Button Clear;
+		private TextBox textBox1;
+		private Label Output;
 	}
 }
--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,38 @@
 namespace AlexsCalculator
 {
-	public partial class Calculator : Form
+	public partial class CalculatorWindow : Form
 	{
-		public Calculator()
+		public CalculatorWindow()
 		{
 			InitializeComponent();
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
 		{
 
 		}
